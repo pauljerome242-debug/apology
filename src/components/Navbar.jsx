a7fb6005b353f4f5c012b0c09db5b2322a0115c1
pauljerome242-debug <pauljerome242-debug@users.noreply.github.com>
2026-09-20 15:42:00 +0530

    Initial website upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav className="w-full px-8 py-4 flex items-center justify-between bg-white/40 backdrop-blur-md shadow-sm">
+      <h1 className="text-2xl font-semibold text-rose-600">
+        Pradeepa💗
+      </h1>
+
+      <p className="text-lg text-purple-600 font-medium">
+        Love you till my last breath💕
+      </p>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/data/story.js
@@ -0,0 +1,28 @@
+import img1 from "../assets/images/IMG_5610.jpeg";
+import img2 from "../assets/images/IMG_5609.jpeg";
+import img3 from "../assets/images/IMG_0457.jpeg";
+import img4 from "../assets/images/IMG_5606.jpeg";
+import img5 from "../assets/images/IMG_5607.jpeg";
+
+export const story = [
+  {
+    image: img4,
+    text: "Hey Pradeepa, I want to tell you something...this story never ends",
+  },
+  {
+    image: img5,
+    text: "I know i've hurt you and you've hurt me too but honestly i've been feeking really bad about everything between us  🥺",
+  },
+  {
+    image: img2,
+    text: "I’m carrying something heavy… not in my hands, but in my heart 🥺",
+  },
+  {
+    image: img1,
+    text: "And I’ve been sitting here for hours with no sleep, trying to find the right words 😌",
+  },
+  {
+    image: img3,
+    text: "I know I let you down, but I want to reassure you that my love for you hasn’t changed for a second. You mean everything to me. Maybe we’ve hurt each other more than we ever wanted to, but beneath all the anger and ego, there’s still a part of me that just wants to hold you close, look into your eyes, kiss you softly, and forget every problem for a while. Will you marry me, Pradeepa? ❤️",
+  },
+];
